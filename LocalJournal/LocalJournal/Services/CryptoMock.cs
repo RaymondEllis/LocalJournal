167863c5c5131a725b421d430bb320d2f9b39fd2
Raymond Ellis <raymondellis89@gmail.com>
2019-10-29 08:21:22 -0700

    Added a couple tests to test the YAML serializer.

---
+++ LocalJournal/LocalJournal/Services/CryptoMock.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace LocalJournal.Services
+{
+	public class CryptoMock : ICrypto
+	{
+		public Task<string> Decrypt(string str)
+		{
+			return Task.FromResult(str + "<DECRYPTED>");
+		}
+
+		public Task<string> Encrypt(string str)
+		{
+			return Task.FromResult(str + "<ENCRYPTED>");
+		}
+
+		public Task<bool> HasKey()
+		{
+			return Task.FromResult(true);
+		}
+
+		public Task StoreKey(string password)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
